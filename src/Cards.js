3a904794f45d5ba8abee46921a273112b6a104ba
tanakatoo <karmen.tanaka@gmail.com>
2023-05-01 19:32:03 -0400

    almost

--- src/Cards.js
+++ src/Cards.js
@@ -9,7 +9,9 @@ const Cards = () => {
     const divRef = useRef()
     const errorRef = useRef()
     const firstRun = useRef(true)
+    let intervalId = useRef()
 
+    const [startTimer, setStartTimer] = useState(false)
     const [cards, setCards] = useState([])
     const [draw, setDraw] = useState(false)
     const [disableButton, setDisableButton] = useState(false)
@@ -18,12 +20,9 @@ const Cards = () => {
         async function getDeck() {
             const res = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
             deckRef.current = res.data.deck_id
-
         }
-
         getDeck()
-
-    }, [])
+    }, [startTimer])
 
     const drawOne = () => {
         firstRun.current = false
@@ -31,14 +30,11 @@ const Cards = () => {
     }
     useEffect(() => {
         async function drawCard() {
-
             if (cards.length == 51) {
                 errorRef.current.innerText = "Error: no cards remaining!"
                 setDisableButton(true)
-                console.log(disableButton)
             } else {
                 const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckRef.current}/draw/?count=1`)
-                console.log(res.data)
                 // put card on the deck
                 const rand = Math.floor(Math.random() * 90)
                 const style = { transform: `rotate(${rand}deg)` }
@@ -50,25 +46,49 @@ const Cards = () => {
 
         if (!firstRun.current) {
             drawCard()
-
         }
-
     }, [draw])
 
 
-    const drawAutomatically = () => {
-        setInterval()
-    }
+    // const [stop, setStop] = useState(false)
+
+
+    useEffect(() => {
+        if (startTimer) {
+            intervalId.current = setInterval(async () => {
+
+                if (cards.length == 10) {
+                    errorRef.current.innerText = "Error: no cards remaining!"
+                    setDisableButton(true)
+                    return clearInterval(intervalId.current)
+                } else {
+                    const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckRef.current}/draw/?count=1`)
+                    // put card on the deck
+                    const rand = Math.floor(Math.random() * 90)
+                    const style = { transform: `rotate(${rand}deg)` }
+                    setCards(cards => {
+                        return [...cards, { img: res.data.cards[0].images.png, code: res.data.cards[0].code, style: style }]
+                    })
+
+                }
+            }, 1000)
+        }
+    }, [startTimer])
+
+
 
     return (
 
         <>
             <div className="Cards-buttons">
                 <p id='error' ref={errorRef} ></p>
-                <button onClick={drawOne} disabled={disableButton}>Gimme a Card!</button><button onClick={drawAutomatically}>Draw every second</button>
+                <button onClick={drawOne} disabled={disableButton}>Gimme a Card!</button><button onClick={() => setStartTimer(true)}>Draw every second</button>
             </div>
             <div className='Cards' ref={divRef}>
-                {cards.map(c => <Card img={c.img} key={c.code} style={c.style} />)}
+                {cards.map(c => {
+                    console.log('running card')
+                    return < Card img={c.img} key={c.code} style={c.style} />
+                })}
             </div>
         </>
     )
